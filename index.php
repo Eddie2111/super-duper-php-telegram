da02b349665bbba06afca15fccae198fa70a4010
unknown <tarek42223@gmail.com>
2021-12-24 16:57:10 +0600

    last commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>DORM Manangement System</title>
+        
+        <link rel = "stylesheet" type="text/css" href="css/home.css">
+    </head>
+    <div id="body">
+        <center>
+            <br/>
+            <div id ="head1">
+                <h1>Welcome to DORM management system</h1>
+            </div>
+        </center>
+        <div id="form">
+            <div id = "section1">
+            <a href = "login.php">
+                <h1> Login  </h1>
+            </a>
+            </div>
+            <div id = "section2">
+            <a href = "registration.php">
+                <h1>Register</h1>
+            </a>
+            </div>
+        </div>
+</div>
+</html>
